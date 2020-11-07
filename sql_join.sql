e2fcc173395ca4405b3cc32c69713e903857b8cb
mcr727 <michael.reichard@gmail.com>
2020-11-07 10:12:34 -0500

    addition fields added to join

--- sql_join.sql
+++ sql_join.sql
@@ -1,4 +1,4 @@
 -- left join from spotify on the track's name
-SELECT billboard.artists, billboard.name, spotify.track_name
+SELECT billboard.artists, billboard.name, spotify.track_name, spotify.album_name, spotify.track_id, spotify.album_name, billboard.peak_position, billboard.writing_credits
 FROM spotify
 LEFT join billboard ON spotify.track_name = billboard.name;
